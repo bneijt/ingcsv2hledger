c0628d6bd30a6ce30490d6e71ed2fb1c13978e36
A. Bram Neijt <bneijt@gmail.com>
2018-01-13 20:25:06 +0100

    The text way

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,53 +1,79 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
+    ( transformIngFileToHLedger
     ) where
 
 import Data.Csv.Parser (csvWithHeader, defaultDecodeOptions)
+import Data.Csv (NamedRecord)
 import Data.Attoparsec.ByteString (parseOnly)
 import qualified Data.ByteString as BS
-import Data.ByteString.Char8 (pack)
-import Data.Csv (NamedRecord)
--- import qualified Data.Text as T
-import qualified Data.Text.Lazy as L
+import Data.HashMap.Strict ((!))
 
-import Data.Text.IO as TIO
--- import Data.Text.Template (substitute)
-import Text.EDE (render, parse, Result(..))
-import Data.HashMap.Lazy (empty, HashMap(..), foldrWithKey, insert)
--- import qualified Data.Text.Lazy as L
 import Data.Text.Encoding (decodeUtf8)
 import qualified Data.Text as T
-import Data.Aeson.Types (Value(..))
+import Data.Text (Text(..))
+import Data.Vector (toList)
+import Data.Monoid ((<>))
+import qualified Data.Text.IO as TIO
+import Data.Text.Read (double)
+
+import TextShow (showt)
+
+-- ["Datum","Naam / Omschrijving","Rekening","Tegenrekening","Code","Af Bij","Bedrag (EUR)","MutatieSoort","Mededelingen"]
+data Transaction = Transaction {
+    dateOfTransaction :: Text,
+    description :: Text,
+    comment :: Text,
+    fromAccount :: Text,
+    toAccount :: Text,
+    amountInEuro :: Double
+} deriving (Show)
+
+
+slice :: Int -> Int -> Text -> Text
+slice from to value = T.take (to - from) part
+    where part = T.drop from value
 
 -- 2008/10/01 take a loan
 --     assets:bank:checking  $1
 --     liabilities:debts    $-1
-ledgerTemplateBody :: BS.ByteString
-ledgerTemplateBody = pack "{{ Datum }}/10/01 {{ Mededelingen }}\n\
-        \    assets:bank:{{ Rekening }}  €{{ amount }}\n"
-
-ledgerTemplate = case parse ledgerTemplateBody of
-    Success template -> template
-    Failure f -> error(show f)
-
-filterMapping :: T.Text -> T.Text -> (T.Text, T.Text)
-filterMapping k v = if k == "Bedrag (EUR)"
-    then ("amount", T.replace "," "." v)
-    else (k, v)
-
-updateWithText :: BS.ByteString -> BS.ByteString -> HashMap T.Text Value -> HashMap T.Text Value
-updateWithText k v map = insert k' (String v') map
+ledgerRecordFrom :: Transaction -> Text
+ledgerRecordFrom transaction =
+    (slice 0 4 td) <> "/" <> (slice 4 6 td) <> "/" <> (slice 6 8 td) <> " " <> (description transaction) <> "\n"
+    <> "    accounts:bank:" <> (fromAccount transaction) <> "   €" <> (showt $ amountInEuro transaction) <> "\n"
+    <> (if T.length (toAccount transaction) > 0 then "    accounts:bank:" <> (toAccount transaction) <> "  €" <> (showt $ -(amountInEuro transaction))  <> "\n" else "")
+    <> "\n"
     where
-        (k', v') = filterMapping (decodeUtf8 k) (decodeUtf8 v)
+        td = (dateOfTransaction transaction)
 
-contextOf record = foldrWithKey updateWithText (empty :: HashMap T.Text Value) record
+doubleValue :: NamedRecord -> BS.ByteString -> Double
+doubleValue r k = readDouble (textValue r k)
+
+textValue :: NamedRecord -> BS.ByteString -> Text
+textValue r k = decodeUtf8 $ r ! k
+
+readDouble :: Text -> Double
+readDouble v =
+    case double v of
+        Right (d, t) -> if T.length t > 0 then error("Still left with text after converting " ++ T.unpack v) else d
+        Left e -> error(e)
 
 
-printLedgerRecordFor :: NamedRecord -> IO()
-printLedgerRecordFor record = case render ledgerTemplate (contextOf record) of
-    Success value -> TIO.putStrLn (L.toStrict value)
-    Failure _ -> print ("FAIL")
+signedDutchEuroFrom :: Text -> Text -> Double
+signedDutchEuroFrom afBij amount = if positive then amountDouble else -amountDouble
+    where
+        amountDouble = readDouble (T.replace "," "." amount)
+        positive = afBij == "Bij"
+
+transactionFromIngRow :: NamedRecord -> Transaction
+transactionFromIngRow row = Transaction {
+    dateOfTransaction = (textValue row "Datum"),
+    description = (textValue row "Naam / Omschrijving"),
+    comment = (textValue row "Mededelingen"),
+    fromAccount = (textValue row "Rekening"),
+    toAccount = (textValue row "Tegenrekening"),
+    amountInEuro = signedDutchEuroFrom (textValue row "Af Bij")(textValue row "Bedrag (EUR)")
+}
 
 
 transformIngFileToHLedger :: FilePath -> IO ()
@@ -56,5 +82,5 @@ transformIngFileToHLedger filepath = do
     case parseOnly (csvWithHeader defaultDecodeOptions) csvContents of
         Left _ -> print "Failed to parse"
         Right (header, rows) -> do
-            mapM_ printLedgerRecordFor rows
-            print header
+            let transactions = map transactionFromIngRow (toList rows)
+            mapM_ (\x -> TIO.putStrLn (ledgerRecordFrom x)) transactions
